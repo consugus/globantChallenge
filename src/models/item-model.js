f188a28a804e815aafd3048023977e1919bceb0a
consugus <consugus@gmail.com>
2020-07-14 07:18:32 -0300

    item-model 'value' column changed to 'sellPrice'. Everything works fine

--- src/models/item-model.js
+++ src/models/item-model.js
@@ -6,7 +6,7 @@ const Item = db.define( 'item', {
     idItem: { type: Sequelize.SMALLINT, primaryKey: true },
     name: { type: Sequelize.STRING },
     description: { type: Sequelize.STRING },
-    value: { type: Sequelize.DECIMAL },
+    sellPrice: { type: Sequelize.DECIMAL },
     currency: { type: Sequelize.STRING },
     createdAt: { type:Sequelize.DATE },
     updatedAt: { type:Sequelize.DATE }
